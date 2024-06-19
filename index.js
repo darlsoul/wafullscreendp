d0e46c08f6dfee3940aad575a526d40f4cf44237
darlsoul <89399174+darlsoul@users.noreply.github.com>
2024-06-19 21:05:44 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,72 @@
+const axios = require('axios');
+const SESSION_NAME = "WaFullPp";
+const express = require('express');
+const cors = require('cors');
+let router = express.Router();
+const pino = require("pino");
+const {
+    default: makeWASocket,
+    useMultiFileAuthState,
+    delay,
+    Browsers,
+    makeCacheableSignalKeyStore
+} = require("@whiskeysockets/baileys");
+
+router.use(cors());
+router.get('/', async (req, res) => {
+    id = "temp";
+    let num = req.query.number;
+
+    async function getPaire() {
+        const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
+        try {
+            let session = makeWASocket({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
+                },
+                printQRInTerminal: false,
+                logger: pino({level: "fatal"}).child({level: "fatal"}),
+                browser: Browsers.macOS("Safari"),
+             });
+
+            if (!session.authState.creds.registered) {
+                await delay(1500);
+                num = num.replace(/[^0-9]/g, '');
+                const code = await session.requestPairingCode(num);
+                if (!res.headersSent) {
+                    await res.send({ code });
+                }
+            }
+
+            session.ev.on('creds.update', saveCreds);
+
+            session.ev.on("connection.update", async (s) => {
+                const { connection, lastDisconnect } = s;
+
+                if (connection == "open") {
+                    await delay(5000);
+                    await delay(5000);
+                    
+                    await session.sendMessage(session.user.id, { text: ` *Successfully Connected*` });
+                    await session.sendMessage(session.user.id, { text: session.user.id });
+
+                    await delay(100);
+                    await session.ws.close();
+                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    getPaire();
+                }
+            });
+        } catch (err) {
+            console.log("service restated");
+            if (!res.headersSent) {
+                await res.send({ code: "Service Unavailable" });
+            }
+        }
+    }
+
+    return await getPaire();
+});
+
+module.exports = router;
