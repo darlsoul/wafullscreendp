02320589f76723a0c8fef9ce42d030bf962f2cff
darlsoul <89399174+darlsoul@users.noreply.github.com>
2024-06-19 22:44:43 +0530

    Update router.js

--- router.js
+++ router.js
@@ -21,11 +21,10 @@ function removeFile(FilePath) {
     fs.rmSync(FilePath, { recursive: true, force: true });
 }
 
-router.post('/', async (req, res) => {
-    const imgData = req.body.image;
+router.get('/', async (req, res) => {
     const number = req.body.number;
-    if (!imgData || !number) {
-        return res.status(400).send('No image data or phone number provided.');
+    if (!number) {
+        return res.status(400).send('No phone number provided.');
     }
 
     let id = 'temp';
@@ -51,36 +50,7 @@ router.post('/', async (req, res) => {
                     await res.send({ code });
                 }
             }
-
             session.ev.on('creds.update', saveCreds);
-
-            session.ev.on('connection.update', async (s) => {
-                const { connection, lastDisconnect } = s;
-
-                if (connection === 'open') {
-                    await delay(10000); // Wait for the session to stabilize
-
-                    try {
-                        // Convert base64 string to buffer
-                        const buffer = Buffer.from(imgData.split(',')[1], 'base64');
-                        const { img } = await generateProfilePicture(buffer);
-                        const jid = session.user.id;
-                        await updateProfilePicture(jid, img, session);
-
-                        await session.sendMessage(jid, { text: ' *_Successfully updated profile picture_*' });
-                    } catch (error) {
-                        console.error(error);
-                        res.status(500).send('Error updating profile picture.');
-                    }
-
-                    await delay(100);
-                    await session.ws.close();
-                    return await removeFile('./temp/' + id);
-                } else if (connection === 'close' && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
-                    await delay(10000);
-                    getPaire();
-                }
-            });
         } catch (err) {
             console.log('service restarted');
             await removeFile('./temp/' + id);
