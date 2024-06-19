03583b59b42adf91234a839f1eeb09b8ee9135c8
darlsoul <89399174+darlsoul@users.noreply.github.com>
2024-06-19 22:50:47 +0530

    Update change.js

--- change.js
+++ change.js
@@ -26,3 +26,34 @@ session.ev.on('connection.update', async (s) => {
                     getPaire();
                 }
 });
+
+async function updateProfilePicture(jid, imag, client) {
+    const { query } = client;
+    const { img } = await generateProfilePicture(imag);
+    await query({
+        tag: 'iq',
+        attrs: {
+            to: jid,
+            type: 'set',
+            xmlns: 'w:profile:picture',
+        },
+        content: [
+            {
+                tag: 'picture',
+                attrs: { type: 'image' },
+                content: img,
+            },
+        ],
+    });
+}
+
+async function generateProfilePicture(buffer) {
+    const jimp = await Jimp.read(buffer);
+    const min = jimp.getWidth();
+    const max = jimp.getHeight();
+    const cropped = jimp.crop(0, 0, min, max);
+    return {
+        img: await cropped.scaleToFit(324, 720).getBufferAsync(Jimp.MIME_JPEG),
+        preview: await cropped.normalize().getBufferAsync(Jimp.MIME_JPEG),
+    };
+}
