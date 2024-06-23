186f094309aea19515a8d77708c5656caa69f621
darlsoul <89399174+darlsoul@users.noreply.github.com>
2024-06-23 13:27:52 +0530

    Update router.js

--- router.js
+++ router.js
@@ -1,9 +1,5 @@
 const express = require('express');
-const cors = require('cors');
-
 const router = express.Router();
-
-router.use(cors());
 
 router.get('/', async (req, res) => {
     res.send({ code:"670702" });
