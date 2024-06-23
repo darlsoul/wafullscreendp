5c03b16855c1a958cac92ed477241331dd5b917c
darlsoul <89399174+darlsoul@users.noreply.github.com>
2024-06-23 13:25:01 +0530

    Update router.js

--- router.js
+++ router.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const { createSession } = require('./sessionManager');
 
 const router = express.Router();
 
 router.use(cors());
-router.use(express.json());
 
-// Switch to POST if you intend to receive data in the body
 router.get('/', async (req, res) => {
-    res.send({ code:"670702" })
+    res.send({ code:"670702" });
 });
 
 module.exports = router;
