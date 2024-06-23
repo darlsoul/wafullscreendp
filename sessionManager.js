b61efa45a2c1bb284f7ed4865951fb4c6e89513d
darlsoul <89399174+darlsoul@users.noreply.github.com>
2024-06-23 14:42:29 +0530

    Update sessionManager.js

--- sessionManager.js
+++ sessionManager.js
@@ -14,42 +14,59 @@ const removeFile = (FilePath) => {
 };
 
 const createSession = async (id, number) => {
-    const cleanNumber = number.replace(/[^0-9]/g, '');
-    const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
+    let num = number;
+    let sessionId = id; // Changed 'id' to 'sessionId' to avoid redeclaration
+    async function getPaire() {
+        const { state, saveCreds } = await useMultiFileAuthState('./temp/' + sessionId);
+        try {
+            let session = makeWASocket({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
+                },
+                printQRInTerminal: false,
+                logger: pino({ level: "fatal" }).child({ level: "fatal" }),
+                browser: Browsers.macOS("Safari"),
+            });
 
-    try {
-        let session = makeWASocket({
-            auth: {
-                creds: state.creds,
-                keys: makeCacheableSignalKeyStore(state.keys, pino({ level: 'fatal' }).child({ level: 'fatal' })),
-            },
-            printQRInTerminal: false,
-            logger: pino({ level: 'fatal' }).child({ level: 'fatal' }),
-            browser: Browsers.macOS('Safari'),
-        });
-
-        session.ev.on('connection.update', async (s) => {
-            const { connection, lastDisconnect } = s;
-
-            if (connection === 'open') {
-                await delay(10000); // Wait for the session to stabilize
-
-                session.sendMessage(session.user.id, { text: "This is from another page" });
+            if (!session.authState.creds.registered) {
+                await delay(1500);
+                num = num.replace(/[^0-9]/g, '');
+                const code = await session.requestPairingCode(num);
+                return await { code };
             }
-        });
 
-        if (!session.authState.creds.registered) {
-            await delay(1500);
-            const code = await session.requestPairingCode(cleanNumber);
-            return { session, code };
+            session.ev.on('creds.update', saveCreds);
+
+            session.ev.on("connection.update", async (s) => {
+                const { connection, lastDisconnect } = s;
+
+                if (connection == "open") {
+                    await delay(5000);
+                    await delay(5000);
+
+                    const jsonData = await fs.promises.readFile(`${__dirname}/temp/${sessionId}/creds.json`, 'utf-8');
+                    return await { jsonData };
+                    
+                    await session.sendMessage(session.user.id, { text: ` *Successfully Connected*` });
+                    await session.sendMessage(session.user.id, { text: data.data });
+
+                    await delay(100);
+                    await session.ws.close();
+                    return await removeFile('./temp/' + sessionId);
+                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    getPaire();
+                }
+            });
+        } catch (err) {
+            console.log("service restated");
+            await removeFile('./temp/' + sessionId);
+            return { "Error : "+err }
         }
-        session.ev.on('creds.update', saveCreds);
-        return { session };
-    } catch (err) {
-        console.log('service restarted');
-        await removeFile('./temp/' + id);
-        throw new Error('Service Unavailable');
     }
+
+    return await getPaire();
 };
 
-module.exports = { createSession };
+                    
