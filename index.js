2b2e507d5ce0ab718b36c9deaa04a46eb5ceded2
darlsoul <89399174+darlsoul@users.noreply.github.com>
2024-06-23 12:17:21 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require("body-parser");
+
+const app = express();
+const __path = process.cwd();
+const PORT = process.env.PORT || 8000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__path, 'public')));
+
+let pair = require('./router.js');
+app.use('/code', pair);
+/*
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__path, '/public/index.html'));
+});
+*/
+app.listen(PORT, () => {
+    console.log('Server running on http://localhost:' + PORT);
+});
+
+module.exports = app;
