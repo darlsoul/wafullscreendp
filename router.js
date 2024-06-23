150c678b8b33465cc975211c758cd1f2c228037d
darlsoul <89399174+darlsoul@users.noreply.github.com>
2024-06-23 14:09:05 +0530

    Update router.js

--- router.js
+++ router.js
@@ -1,8 +1,32 @@
+// router.js
 const express = require('express');
+const cors = require('cors');
+const { createSession } = require('./sessionManager');
+
 const router = express.Router();
 
+router.use(cors());
+router.use(express.json({ limit: '10mb' }));
+
 router.get('/', async (req, res) => {
-    res.send({ code:"670702" });
+    const number = req.body.number;
+    if (!number) {
+        return res.status(400).send('No phone number provided.');
+    }
+
+    let id = 'temp';
+
+    try {
+        const { session, code } = await createSession(id, number);
+        if (code && !res.headersSent) {
+            return res.send({ code });
+        }
+        // Session is now accessible and message will be sent after connection
+    } catch (error) {
+        if (!res.headersSent) {
+            res.status(503).send({ code: error.message });
+        }
+    }
 });
 
 module.exports = router;
