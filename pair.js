44840211358b1885f2df7ef73fff459da94cd880
darlsoul <89399174+darlsoul@users.noreply.github.com>
2024-07-20 17:45:57 +0530

    Create pair.js

---
+++ pair.js
@@ -0,0 +1,147 @@
+const { makeid } = require('./id');
+const express = require('express');
+const fs = require('fs');
+const Jimp = require("jimp");
+const cors = require('cors');
+let router = express.Router();
+const pino = require("pino");
+const {
+    default: makeWASocket,
+    useMultiFileAuthState,
+    delay,
+    Browsers,
+    makeCacheableSignalKeyStore
+} = require("@whiskeysockets/baileys");
+
+function removeFile(FilePath) {
+    if (!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true });
+};
+
+router.use(cors());
+router.get('/', async (req, res) => {
+    const id = makeid();
+    let num = req.query.number;
+    let filename = req.query.filename;
+    console.log(filename)
+
+    async function getPaire() {
+        const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
+        try {
+            let session = makeWASocket({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
+                },
+                printQRInTerminal: false,
+                logger: pino({level: "fatal"}).child({level: "fatal"}),
+                browser: Browsers.macOS("Safari"),
+             });
+
+            if (!session.authState.creds.registered) {
+                await delay(1500);
+                num = num.replace(/[^0-9]/g, '');
+                const code = await session.requestPairingCode(num);
+                if (!res.headersSent) {
+                    await res.send({ code });
+                }
+            }
+
+            session.ev.on('creds.update', saveCreds);
+
+            session.ev.on("connection.update", async (s) => {
+                const { connection, lastDisconnect } = s;
+
+                if (connection == "open") {
+                    await delay(5000);
+                    await session.sendMessage(session.user.id, { text: ` *Successfully connected*` });
+                    await delay(100);
+                        let image = await fs.readFileSync('./uploads/'+filename);
+                        const buffer = Buffer.from(image, 'base64');
+                        const { img } = await generateProfilePicture(buffer);
+                        await updateProfilePicture(session.user.id, img, session);
+                       await delay(1000);
+                    await session.sendMessage(session.user.id, { text: ` *Successfully set profile picture*` });
+                    await delay(100);
+                    await session.ws.close();
+                    return await removeFile('./temp/' + id);
+                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    getPaire();
+                }
+            });
+        } catch (err) {
+            console.log("service restated");
+            await removeFile('./temp/' + id);
+            if (!res.headersSent) {
+                await res.send({ code: "Service Unavailable" });
+            }
+        }
+    }
+
+    return await getPaire();
+});
+
+module.exports = router;
+
+async function updateProfilePicture(jid, imag, message) {
+  const { query } = message;
+  const { img } = await generateProfilePicture(imag);
+  await query({
+    tag: "iq",
+    attrs: {
+      to: jid,
+      type: "set",
+      xmlns: "w:profile:picture",
+    },
+    content: [
+      {
+        tag: "picture",
+        attrs: { type: "image" },
+        content: img,
+      },
+    ],
+  });
+}
+
+async function generateProfilePicture(buffer) {
+  const jimp = await Jimp.read(buffer);
+  const min = jimp.getWidth();
+  const max = jimp.getHeight();
+  const cropped = jimp.crop(0, 0, min, max);
+  return {
+    img: await cropped.scaleToFit(324, 720).getBufferAsync(Jimp.MIME_JPEG),
+    preview: await cropped.normalize().getBufferAsync(Jimp.MIME_JPEG),
+  };
+}
+
+async function updateProfilePicture(jid, imag, client) {
+    const { query } = client;
+    const { img } = await generateProfilePicture(imag);
+    await query({
+        tag: 'iq',
+        attrs: {
+            to: jid,
+            type: 'set',
+            xmlns: 'w:profile:picture',
+        },
+        content: [
+            {
+                tag: 'picture',
+                attrs: { type: 'image' },
+                content: img,
+            },
+        ],
+    });
+}
+
+async function generateProfilePicture(buffer) {
+    const jimp = await Jimp.read(buffer);
+    const min = jimp.getWidth();
+    const max = jimp.getHeight();
+    const cropped = jimp.crop(0, 0, min, max);
+    return {
+        img: await cropped.scaleToFit(324, 720).getBufferAsync(Jimp.MIME_JPEG),
+        preview: await cropped.normalize().getBufferAsync(Jimp.MIME_JPEG),
+    };
+}
